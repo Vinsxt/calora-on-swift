eafdc27c274f3eaf73a68d2b010afac3db541775
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-08-18 22:09:50 +0700

    Managed to arrange HStacks and VStacks to set simple UI design box

--- Calora-swift/Views/HomePageView.swift
+++ Calora-swift/Views/HomePageView.swift
@@ -15,10 +15,63 @@ struct HomePageView: View {
     
     
     var body: some View {
-        Text("Hello, \(profiles.first?.name ?? "Unknown")")
-        Text("Weight: \(profiles.first?.weight ?? 0)")
-        Text("Height: \(profiles.first?.height ?? 0)")
-        Text("Gender: \(profiles.first?.selectedGender ?? "Not specified")")
+        
+        VStack(alignment: .center){
+            
+            Text("Hello, \(profiles.first?.name ?? "Unknown")")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+            VStack(spacing: 12){
+                
+                    HStack(spacing: 12){
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.white)
+                            .frame(maxHeight: 70)
+                      
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.white)
+                            .frame(maxHeight: 70)
+
+                    }.padding(.horizontal, 0.1)
+                    
+                    HStack(spacing: 12){
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.white)
+                            .frame(maxHeight: 70)
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.white)
+                            .frame(maxHeight: 70)
+
+                    }.padding(.horizontal, 0.1)
+                }
+                .padding(20)
+                .background(RoundedRectangle(cornerRadius: 10)
+                    .fill(.gray.opacity(0.2)))
+                    
+            
+            Text("Weight: \(profiles.first?.weight ?? 0)")
+                .font(.title)
+                .bold(true)
+                
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("Height: \(profiles.first?.height ?? 0)")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("Gender: \(profiles.first?.selectedGender ?? "Not specified")")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+            
+        }.padding(.horizontal, 35)
+            .padding(.top)
+        
+        
+        
+        
+        
+        
     }
 }
 
